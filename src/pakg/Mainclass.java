b5c05d7c6962002b1eda5fdcab6159bc4941206c
Nvidia Geforce <Nvidia Geforce@172.16.105.239>
2016-05-30 05:49:16 -0700

    m shehzad

---
+++ src/pakg/Mainclass.java
@@ -0,0 +1,49 @@
+package pakg;
+
+import java.util.Scanner;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		       double n1, n2;
+		        String operation;
+		        Scanner scannerObject = new Scanner(System.in);
+		
+		        System.out.println("Enter first number");
+		        n1 = scannerObject. nextDouble();
+		
+		        System.out.println("Enter second number");
+		        n2 = scannerObject. nextDouble();
+		
+		        Scanner op = new Scanner(System.in);
+		        System.out.println("Enter your operation");
+		        operation = op.next();
+		
+		        switch (operation)  {
+		        case "+":
+		            System.out.println("Your answer is " + (n1 + n2));
+		            break;
+		
+		        case "-":
+		            System.out.println("Your answer is " + (n1 - n2));
+		            break;
+		
+		        case "/":
+		            System.out.println("Your answer is " + (n1 / n2));
+		            break;
+		
+		        case "*":
+		            System.out.println("Your asnwer is " + (n1 * n2));
+		            break;
+		
+		        default:
+		            System.out.println("Je ne sais pas");
+		
+		        }
+		    }
+		
+	}
+
+
